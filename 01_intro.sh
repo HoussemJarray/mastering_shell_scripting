0c0a8cecf11ba95c97ce26748a08e6f0cb5f98f0
Houssem-Jarray <houssemjarray315@gmail.com>
2026-07-09 21:02:00 +0100

    writing first script shell

---
+++ 01_intro.sh
@@ -0,0 +1,12 @@
+echo "==== Welcome to your first script shell session ===="
+echo "The current user of this session is :"
+whoami
+
+echo ""
+echo "=== Today's day is :"
+date 
+
+echo "=== The default interpreter used in this Operating System is :"
+echo $SHELL 
+
+echo "======================================================"
