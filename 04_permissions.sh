04e67fa9d08cb2b2e8ed2b61600aa68515ac9bbe
Houssem-Jarray <houssemjarray315@gmail.com>
2026-07-10 00:37:40 +0100

    understand permissions

---
+++ 04_permissions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e 
+
+
+
+  
+echo "=== Permissions ==="
+echo "=== the current file Permissions is :==="
+echo "-rw-rw-r--"
+echo "-   ===> is a regular file"
+echo "rw- ===> the owner Permissions"
+echo "rw- ===> the group Permissions"
+echo "r-- ===> the others Permissions"
+
+
+
