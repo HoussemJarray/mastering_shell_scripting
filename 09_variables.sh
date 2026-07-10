4f6fc8680519cb7b0331422a306e23a0e8480642
Houssem-Jarray <houssemjarray315@gmail.com>
2026-07-10 11:01:44 +0100

    Working with variables

---
+++ 09_variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e 
+
+echo "################################################"
+echo "#         Mastering shell scripting            #"
+echo "#         _________________________            #"
+echo "#              variables.sh                    #"
+echo "################################################"
+
+name="09_variables.sh"
+session=9
+readonly PI=3.1459
+
+echo "welcome to the new session $name number $session"
+echo "PI=$PI"
+
+#PI=3.14
+
+
